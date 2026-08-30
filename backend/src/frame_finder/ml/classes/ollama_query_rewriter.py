f240dbe231d8111f7bb666d13ff7ecfeb1947cb8
chiz.nk <chzba333@gmail.com>
2026-08-30 01:02:15 +0100

    Switched back to accessing ollama via the module

--- backend/src/frame_finder/ml/classes/ollama_query_rewriter.py
+++ backend/src/frame_finder/ml/classes/ollama_query_rewriter.py
@@ -1,35 +1,30 @@
 from src.frame_finder.ml.interfaces.query_rewriter import QueryRewriter
 from src.frame_finder.files import load_text
-from dotenv import load_dotenv
-import requests, os
-load_dotenv()
+from ollama import Client
+import os
 
 class OllamaQueryRewriter(QueryRewriter):
     _SYSTEM_PROMPT = load_text("src.frame_finder.config", "slm_system_prompt.txt")
-    _OLLAMA_URL = os.getenv("OLLAMA_URL")
 
     def __init__(self, model: str):
         self._model = model
+        self._client = Client(os.getenv("OLLAMA_URL"))
 
     def rewrite(self, query: str) -> str:
-        response = requests.post(
-            f"{self._OLLAMA_URL}/api/generate",
-            json={
-                "model": self._model,
-                "messages": [
-                    {
-                        "role": "system",
-                        "content": self._SYSTEM_PROMPT,
-                    },
-                    {
-                        "role": "user",
-                        "content": query,
-                    },
-                ],
-                "stream": False,
-                "options": {
-                    "temperature": 0,
+        response = self._client.chat(
+            model=self._model,
+            messages=[
+                {
+                    "role": "system",
+                    "content": self._SYSTEM_PROMPT,
                 },
+                {
+                    "role": "user",
+                    "content": query,
+                },
+            ],
+            options={
+                "temperature": 0,
             },
         )
 
