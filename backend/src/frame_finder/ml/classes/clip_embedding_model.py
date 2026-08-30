65d502a5fc6fab6bb0a6a5c1a28015c611fc607d
chiz.nk <chzba333@gmail.com>
2026-08-30 16:58:57 +0100

    Redis queue factory now uses .env for host url

--- backend/src/frame_finder/ml/classes/clip_embedding_model.py
+++ backend/src/frame_finder/ml/classes/clip_embedding_model.py
@@ -7,25 +7,16 @@ load_dotenv()
 
 class CLIPEmbeddingModel(EmbeddingModel):
     def __init__(self, model: str, device: str | None = None):
-        print("1")
         self._device = device or (
             "cuda" if torch.cuda.is_available() else "cpu"
         )
-        print("2")
         hf_token = os.getenv("HF_TOKEN")
-        print("3")
+
         try:
-            print("4")
             self._processor = CLIPProcessor.from_pretrained(model, token=hf_token)
-            print("5")
-            self._model = CLIPModel.from_pretrained(model, token=hf_token)
-            print("6")
-            self._model = self._model.to(self._device)
-            print("7")
+            self._model = CLIPModel.from_pretrained(model, token=hf_token).to(self._device)
             self._model.eval()
-            print("8")
         except Exception as e:
-            print(e)
             raise ValueError(f"Invalid or unsupported model: {model}") from e
 
     @torch.no_grad()
--- backend/src/frame_finder/pipeline/classes/redis_queue_factory.py
+++ backend/src/frame_finder/pipeline/classes/redis_queue_factory.py
@@ -2,12 +2,14 @@ from src.frame_finder.pipeline.classes.redis_queue import RedisQueue
 from src.frame_finder.pipeline.interfaces.worker_queue import WorkerQueue
 from src.frame_finder.pipeline.interfaces.worker_queue_factory import WorkerQueueFactory
 from redis import Redis
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 class RedisQueueFactory(WorkerQueueFactory):
     def new(self) -> WorkerQueue:
         redis = Redis(
-            host="localhost",
-            port=6379,
+            host=os.getenv("REDIS_URL")
         )
 
         return RedisQueue(
